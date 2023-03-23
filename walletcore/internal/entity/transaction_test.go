615742d11ac66e2978fc67001071b151cc8b956f
Richard <richard_bueno@ymail.com>
2023-03-22 21:38:48 -0300

    first commit

---
+++ walletcore/internal/entity/transaction_test.go
@@ -0,0 +1,40 @@
+package entity
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCreateTransaction(t *testing.T) {
+	client1, _ := NewClient("rich 1", "rich@com")
+	account1 := NewAccount(client1)
+	client2, _ := NewClient("john doe", "j@j.com")
+	account2 := NewAccount(client2)
+
+	account1.Credit(1000)
+	account2.Credit(1000)
+
+	transaction, err := NewTransaction(account2, account1, 100)
+	assert.Nil(t, err)
+	assert.NotNil(t, transaction)
+	assert.Equal(t, 1100.0, account1.Balance)
+	assert.Equal(t, 900.0, account2.Balance)
+}
+
+func TestCreateTransactionWithoutBalance(t *testing.T) {
+	client1, _ := NewClient("rich 1", "rich@com")
+	account1 := NewAccount(client1)
+	client2, _ := NewClient("john doe", "j@j.com")
+	account2 := NewAccount(client2)
+
+	account1.Credit(1000)
+	account2.Credit(1000)
+
+	transaction, err := NewTransaction(account2, account1, 2000)
+	assert.NotNil(t, err)
+	assert.Nil(t, transaction)
+	assert.Equal(t, 1000.0, account1.Balance)
+	assert.Equal(t, 1000.0, account2.Balance)
+	assert.Error(t, err, "insufficient funds")
+}
---
+++ walletcore/internal/usecase/createAccount/create_account.go
@@ -0,0 +1,39 @@
+package createaccount
+
+import (
+	"github.com/rbueno/fc-ms-wallet/internal/entity"
+	"github.com/rbueno/fc-ms-wallet/internal/gateway"
+)
+
+type CreateAccountDTO struct {
+	ClientID string
+}
+
+type CreateAccountOutputDTO struct {
+	ID string
+}
+
+type CreateAccountUseCase struct {
+	AccountGateway gateway.AccountGateway
+	ClientGateway  gateway.ClientGateway
+}
+
+func NewCreateAccountUseCase(a gateway.AccountGateway, c gateway.ClientGateway) *CreateAccountUseCase {
+	return &CreateAccountUseCase{AccountGateway: a, ClientGateway: c}
+}
+
+func (uc *CreateAccountUseCase) Execute(input CreateAccountDTO) (*CreateAccountOutputDTO, error) {
+	client, err := uc.ClientGateway.Get(input.ClientID)
+	if err != nil {
+		return nil, err
+	}
+	account := entity.NewAccount(client)
+	err = uc.AccountGateway.Save(account)
+	if err != nil {
+		return nil, err
+	}
+
+	return &CreateAccountOutputDTO{
+		ID: account.ID,
+	}, nil
+}
